53907a9f5c496c8eea81d367b27a82ebb5c653fa
Tiiilod <turky2202@student.abudzar.sch.id>
2025-05-05 11:35:04 +0700

    update

--- kolaborasi/src/main.jsx
+++ kolaborasi/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './projek/projek'
+
+
+import App from './projek'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
