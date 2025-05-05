de6adde15d14c83e3d16a3ce0f13809875cee676
Glng01rmdhn <galang2909@student.abudzar.sch.id>
2025-05-05 14:29:43 +0800

    ckckck

--- kolaborasi/src/projek/projek.jsx
+++ kolaborasi/src/projek/projek.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center justify-start p-8">
         <h1 className="text-4xl font-blod text-gray-800 mb-6"> ini punya galang</h1>
-        <img src={fotogalang} alt="foto galang" alt="foto besar" className=""/>
+        <img src={fotogalang} alt="foto galang" className=""/>
     </div>
   );
 }
